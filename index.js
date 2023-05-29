24546c70ba9e6725948e219893fdf0731a26b333
BrenoBar51 <brenobarcelos9@gmail.com.com>
2023-05-29 10:05:44 -0300

    novo

--- index.js
+++ index.js
@@ -3,10 +3,16 @@ const btn = document.getElementById('btn')
 const openModal = document.getElementById('openModal')
 const fechar = document.getElementById('fechar')
 const closeModal = document.querySelector('#closeModal')
+const enviar = document.getElementById('enviar')
 
 function handleModal() {
     modal.classList.toggle('modalOpen')
 }
 
+function alerta(){
+    alert("Enviado!")
+}
+
 btn.addEventListener("click", handleModal)
 fechar.addEventListener("click", handleModal)
+enviar.addEventListener("click", alerta)
