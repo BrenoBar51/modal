836acf6c2ee77b60c8350b69ba3876f94ed4d2c7
BrenoBar51 <brenobarcelos9@gmail.com.com>
2023-05-29 09:27:01 -0300

    d

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 const modal = document.querySelector('#modal')
+const enviar = document.getElementById('enviar')
 function handleModal() {
     modal.classList.toggle('modalOpen')
 }
+function alerta(){
+    alert("Enviado")
+}
+input.addEventListener("click",() => {
+    if(true){
+        console.log('click')
+    }
+});
